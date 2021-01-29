d41e73b31fabf59310dc9c32a146ebb6880597aa
William Limestall <wlimesta@hawk.iit.edu>
2021-01-29 11:40:46 -0600

    Initial Commit

---
+++ Shared/PlottingTESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlottingTESTApp.swift
+//  Shared
+//
+//  Created by William Limestall on 1/29/21.
+//
+
+import SwiftUI
+
+@main
+struct PlottingTESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
